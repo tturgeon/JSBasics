fbad285c86e61201fb1725bb9fd0c74403d3e133
Georgie Kirschner <georg.kirschner@icloud.com>
2017-09-28 20:07:21 -0600

    removeDuplicate test

--- test/arrays/removeDuplicates.test.js
+++ test/arrays/removeDuplicates.test.js
@@ -4,6 +4,12 @@ var removeDuplicates = require('../../arrays/removeDuplicates.js');
 var expect = chai.expect;
 var assert = chai.assert;
 
-describe("Array Exercise - removeDuplicates()", function(){
-
+describe.only("Array Exercise - removeDuplicates()", function(){
+  it('should remove duplicates from arr', function () {
+    var arr = [1, 2, 2, 3, 4, 5, 5];
+    var noDubsArray = removeDuplicates(arr, 4);
+    expect(noDubsArray.length).to.eql(5);
+    expect(noDubsArray[2]).to.eql(3);
+    expect(noDubsArray[4]).to.eql(5);
+  })
 });
